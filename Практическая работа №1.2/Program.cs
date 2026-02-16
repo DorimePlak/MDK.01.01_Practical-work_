038db3e3211722c78672ecd7f2a239cd053781d1
DorimePlak <naumova04.06.08@gmail.com>
2026-02-16 16:38:00 +0300

    first commit

---
+++ Практическая работа №1.2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Практическая_работа__1._2
+{
+    class Program_7
+    {
+        static void Main()
+        {
+            Console.Write("Введите трёхзначное целое число: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int temp = number;
+            int sum = 0;
+
+            while (temp > 0)
+            {
+                sum += temp % 10;
+                temp /= 10;
+            }
+
+            int digit1 = number / 100;
+            int digit2 = (number / 10) % 10;
+            int digit3 = number % 10;
+            Console.WriteLine($"Сумма цифр числа: {digit1} + {digit2} + {digit3} = {sum}");
+        }
+    }
+} //Задание номер 7
